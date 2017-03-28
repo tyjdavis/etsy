1e2f2cba9e82201491cfabc0f726d2f4aabe41ae
Tyler Davis <tyjdavis@outlook.com>
2017-03-28 16:26:49 -0400

    #3 solved

--- index.js
+++ index.js
@@ -16,20 +16,26 @@ console.log("1. The average Price is $",average.toPrecision(4));
 
 //#2
 
+
 let shirtCount = items.filter(object => object.price >= 14.00 && object.price <= 18.00)
-.map(function (item) {return item.title;});
+.map(function (item) {return [item.title, item.price]});
 
 console.log("2. The array is", shirtCount);
 
 
 //#3
 
-.map(function (item) {return item.})
 
-console.log("3. The item is", fooFunction());
+let gbpItem = items.filter(object => object.currency_code === "GBP")
+.map(function (item) {return [item.title, item.price]})
+
+console.log("3. The item is", gbpItem);
 
 
-//console.log("4. Here is the list", fooFunction());
+//#4
+
+
+console.log("4. Here is the list", fooFunction());
 
 
 
